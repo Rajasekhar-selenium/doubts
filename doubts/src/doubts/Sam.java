23e7e38ffcdaef4ba5b49287b80bc0f962ecf5f7
mailboxskumar <mailbox.skumar@gmail.com>
2019-04-19 19:45:15 +0530

    type any message

---
+++ doubts/src/doubts/Sam.java
@@ -0,0 +1,28 @@
+package doubts;
+
+public class Sam 
+{
+	public void one()
+	{
+		int i = 10; int j = 20;
+		System.out.println(i+j);
+		System.out.println("one");
+	}
+	
+	public void two()
+	{
+		
+		System.out.println("two");
+	}
+	
+	public void three()
+	{
+		System.out.println("three");
+	}
+	
+	public  static void main(String[] args)
+	{
+		
+	}
+
+}
---
+++ doubts/src/doubts/Sample.java
@@ -0,0 +1,39 @@
+package doubts;
+
+public class Sample extends Sam
+{
+	
+
+	public void one()
+	{
+		System.out.println("four");
+	}
+	
+	 public void two()
+	{
+
+		System.out.println("five");
+	
+	}
+	
+	public void three()
+	{
+		System.out.println("six");
+	}
+	
+	public static void main(String[] args) 
+	{
+		Sample a = new Sample();
+		
+		//int i = 345643111;
+		
+		a.one();
+		
+		a.two();
+		
+		a.three();
+		
+		
+	}
+
+}
